ddf0c6204e94b2cfe2534e15f865334fb3e028d2
unknown <zhaojonathan99@gmail.com>
2020-05-22 11:33:46 -0700

    added arguments to running

--- Main.java
+++ Main.java
@@ -10,15 +10,25 @@ import javax.imageio.ImageIO;
 import java.awt.Component;
 import java.awt.image.BufferedImage;
 
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 
 public class Main{
-	static final int size = 1000;
-	static final int numTiles = 200;
+	static final int DEF_SIZE = 1000;
+	static final int DEF_TILES = 200;
+	static final int DEF_COMPLEXITY = 100;
 	static JFrame frame;
+
+	static int size = DEF_SIZE;
+	static int numTiles = DEF_TILES;
+	static int complexity = DEF_COMPLEXITY;
+	static boolean animate = false;
+	static boolean minimized = false;
+	static String fileName = "pattern";
 
 	// ripped from stackoverflow
 	public static BufferedImage getScreenShot(Component component){
@@ -27,26 +37,98 @@ public class Main{
 		return image;
 	}
 
-	public static void main(String[] args){
-		System.out.println("If you want it to save, run with args:");
-		System.out.println("Arg 0: suffix of file name");
-		System.out.println("Arg 1: any input will make it quit upon saving (so no visualization of the pattern)");
+	public static void main(String[] rArgs){
+		System.out.println("Please type in arguments as (ARG NAME)=(ARG VALUE)");
+		System.out.println("Args descriptions:");
+		System.out.println("RES: Total resolution (int)");
+		System.out.println("TILES: How many tiles to render (int)");
+		System.out.println("CMPLX: Network complexity (int)");
+		System.out.println("ANIM: If rendering should be animated (true/false)");
+		System.out.println("MIN: If window should never pop up (true/false)");
+		System.out.println("FILE: Screenshot and network file names (string)");
+		ArrayList<String> args = new ArrayList<String>(Arrays.asList(rArgs));
+		args.ensureCapacity(6);
+		args.forEach(obj -> {
+			String str = (String) obj;
+
+			if(str == null){
+				return;
+			} else if(str.contains("RES=")){
+				
+				try{
+					size = Integer.parseInt(str.substring(4, str.length()));
+				} catch(Exception e){
+					System.out.println("Could not read RES");
+					size = DEF_SIZE;
+				}
+
+			} else if(str.contains("TILES=")){
+
+				try{
+					numTiles = Integer.parseInt(str.substring(6, str.length()));
+				} catch(Exception e){
+					System.out.println("Could not read TILES");
+					numTiles = DEF_TILES;
+				}
+
+			} else if(str.contains("CMPLX=")){
+
+				try{
+					complexity = Integer.parseInt(str.substring(6, str.length()));
+				} catch(Exception e){
+					System.out.println("Could not read CMPLX");
+					complexity = DEF_COMPLEXITY;
+				}
+
+			} else if(str.contains("ANIM=")){
+				
+				try{
+					animate = Boolean.parseBoolean(str.substring(5, str.length()));
+				} catch(Exception e){
+					System.out.println("Could not read ANIM");
+					animate = false;
+				}
+
+			} else if(str.contains("MIN=")){
+
+				try{
+					minimized = Boolean.parseBoolean(str.substring(4, str.length()));
+				} catch(Exception e){
+					System.out.println("Could not read MIN");
+					minimized = false;
+				}
+
+			} else if(str.contains("FILE=")){
+
+				try{
+					fileName = str.substring(5, str.length());
+				} catch(Exception e){
+					System.out.println("Could not read FILE");
+					fileName = "pattern";
+				}
+
+			}
+
+		});
+
 		frame = new JFrame("Canvas");
 		frame.setSize(size, size);
 
-		if(args.length > 1){
+		if(minimized){
 			frame.setState(frame.ICONIFIED);
 		}
 
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		if(animate){
+			frame.setVisible(true);
+		}
 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new GridLayout(numTiles, numTiles));
 
 		Genome test = new Genome(new HashMap<Integer, Gene>(), 2, 3);
 		test.compile();
 		
-		for(int i = 0; i < 100; i++){
+		for(int i = 0; i < DEF_COMPLEXITY; i++){
 
 			if(Math.random() >= 0.5){
 				test.generateLink();
@@ -109,23 +191,23 @@ public class Main{
 
 		frame.validate();
 
-		if(args.length > 0){
+		if(!animate){
+			frame.setVisible(true);
+		}
 
-			BufferedImage img = getScreenShot(frame.getContentPane());
+		BufferedImage img = getScreenShot(frame.getContentPane());
 
-			try{
-				ImageIO.write(img, "png", new File("patterns/screenshot" + args[0] + ".png"));
-				ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("patterns/network" + args[0] + ".gen"));
-				oos.writeObject(test);
-				oos.close();
-			} catch(Exception e){
-				e.printStackTrace();
-			}
+		try{
+			ImageIO.write(img, "png", new File("patterns/" + fileName + ".png"));
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("patterns/" + fileName + ".gen"));
+			oos.writeObject(test);
+			oos.close();
+		} catch(Exception e){
+			e.printStackTrace();
+		}
 
-			if(args.length > 1){
-				System.exit(0);
-			}
-
+		if(minimized){
+			System.exit(0);
 		}
 
 	}
@@ -141,8 +223,11 @@ class JCanvas extends JPanel{
 	public JCanvas(int size, int r, int g, int b){
 		this.size = size;
 		this.r = Math.min(254, r);
+		this.r = Math.max(0, this.r);
 		this.g = Math.min(254, g);
+		this.g = Math.max(0, this.g);
 		this.b = Math.min(254, b);
+		this.b = Math.max(0, this.b);
 		setPreferredSize(new Dimension(size, size));
 	}
 
